ba5728a69e5b4fdbf10084fae015d19b3668a224
Ricardo Baumann <gemini.richard@gmail.com>
2017-09-09 11:57:16 +0200

    memory CRUD

--- hello.go
+++ hello.go
@@ -2,32 +2,59 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"time"
+	"encoding/json"
+	"io/ioutil"
 )
+
+type Todo struct {
+	Name      string `json:"name"`
+	Completed bool `json:"completed"`
+	Due       time.Time `json:"due"`
+}
+
+type Todos []Todo
+
+var members = []Todo{{Name: "first"}}
+
+var todosMap = make(map[string]Todo)
 
 func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
-	router.HandleFunc("/todos", TodoIndex)
-	router.HandleFunc("/todos/{todoId}", TodoShow)
+	router.HandleFunc("/todos", TodoIndex).Methods("GET")
+	router.HandleFunc("/todos/{todoId}", TodoShow).Methods("GET")
+	router.HandleFunc("/todos/{todoId}", TodoPut).Methods("PUT")
+	router.HandleFunc("/todos/{todoId}", TodoDelete).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
+func TodoDelete(writer http.ResponseWriter, request *http.Request) {
+	delete(todosMap, mux.Vars(request)["todoId"])
+	writer.WriteHeader(http.StatusNoContent)
+}
+func TodoPut(writer http.ResponseWriter, request *http.Request) {
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	var t Todo
+	b, _ := ioutil.ReadAll(request.Body)
+	json.Unmarshal(b, &t)
+	members = append(members, t)
+	todosMap[mux.Vars(request)["todoId"]] = t
+	j, _ := json.Marshal(t)
+	writer.Write(j)
 }
 
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Todo Index!")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todosMap)
 }
 
 func TodoShow(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(r)
-	todoId := vars["todoId"]
-	fmt.Fprintln(w, "Todo show:", todoId)
+	todo := todosMap[vars["todoId"]]
+	fmt.Fprintln(w, "Todo show:", todo)
 }
